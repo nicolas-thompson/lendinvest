e916960ed1739f47b19a80609a23941ffeb58d7d
Nicolas Thompson <mail@nicolasthompson.com>
2019-09-05 21:46:59 +0100

    investor can invest one thousand pounds in tranche

--- tests/Feature/WalletTest.php
+++ tests/Feature/WalletTest.php
@@ -13,10 +13,27 @@ class WalletTest extends TestCase
     /** @test */
     public function each_investor_has_one_thousand_pounds_in_thier_virtual_wallet()
     {
-        $investor= factory('App\User')->create();
+        $investor = factory('App\User')->create();
 
         factory('App\Wallet')->create(['user_id' => $investor->id]);
 
         $this->assertEquals(1000, $investor->wallet->balance);
+    }
+
+    /** @test */
+    public function an_investor_can_invest_one_thousand_pounds_in_a_tranche()
+    {
+        $investor = factory('App\User')->create();
+
+        factory('App\Wallet')->create(['user_id' => $investor->id]);
+
+        $loan = factory('App\Loan')->create([
+            'start' => '01/10/2015',
+            'end' => '15/11/2015',
+        ]);
+
+        $tranche = factory('App\Tranche')->create(['loan_id' => $loan->id]);
+
+        $tranche->invest($investor->wallet, $amount = 1000);
     }
 }
