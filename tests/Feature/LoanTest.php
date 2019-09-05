1207799ef66de345607f300d7a81a136a128fe6b
Nicolas Thompson <mail@nicolasthompson.com>
2019-09-05 18:33:46 +0100

    adds and_a_loan_has_a_tranche test

--- tests/Feature/LoanTest.php
+++ tests/Feature/LoanTest.php
@@ -14,8 +14,18 @@ class LoanTest extends TestCase
     public function given_we_have_a_loan()
     {
         $loan = factory('App\Loan')->create();
-        
+
         // given we have a loan
         $this->assertInstanceOf('App\Loan', $loan);
+    }
+
+    /** @test */
+    public function and_a_loan_has_a_tranche()
+    {
+        $loan = factory('App\Loan')->create();
+        
+        $tranche = factory('App\Tranche')->create(['loan_id' => $loan->id]);
+
+        $this->assertEquals($tranche->id, $loan->tranche->first()->id);
     }
 }  
