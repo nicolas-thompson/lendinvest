dd49ab54dc2fd215bbcdc8f9b3446a84a12686f2
Nicolas Thompson <mail@nicolasthompson.com>
2019-09-07 12:50:01 +0100

    casts wallet balance to integer

--- app/Wallet.php
+++ app/Wallet.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Wallet extends Model
 {
+    protected $casts = [
+        'balance' => 'int',
+    ];
+
     public function debitBalance($amount)
     {
         $this->balance = $this->balance - $amount;
