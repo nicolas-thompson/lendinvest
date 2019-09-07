bdb54b49d97cd92fa9d394d580ded236d62e8556
Nicolas Thompson <mail@nicolasthompson.com>
2019-09-07 13:57:28 +0100

    adds exceptions to can invest method on tranche

--- app/Tranche.php
+++ app/Tranche.php
@@ -2,25 +2,38 @@
 
 namespace App;
 
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 
 class Tranche extends Model
 {
-    public function canInvest($wallet, $amount, $now) : bool
+    public function canInvest($wallet, $amount, $now)
     {
-        if($wallet->balance < $amount) {
-            return false;
-        }
+        try {
+
+            if($wallet->balance < $amount) {
+                throw new \Exception('The wallet balance is smaller than the amount.'); 
+            }   
+ 
+         } catch (\Exception $e) {
+            return response($e, 422);
+         }
         
-        if($amount > $this->balance) {
-            return false;
-        }
+         try {
 
+            if($amount > $this->balance) {
+                throw new \Exception('The amount is larger than the tranche balance.'); 
+            }   
+ 
+         } catch (\Exception $e) {
+            return response($e, 422);
+         }
+        
         if ($this->open() && $this->loan->open($now)) {
-            return true;
+            return response('yes', 200);
         }
 
-        return false;
+        return response($e, 422);
     }
 
     public function debitBalance($amount)
